72862633b2052d56c7c7298d0d1d72326be969f3
TobiahRex <tobiah.rex@gmail.com>
2016-10-27 01:36:30 -0700

    modified Bank schema

--- server/models/Bank.js
+++ server/models/Bank.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 
 const bankSchema = new mongoose.Schema({
-  name: { type: String },
-  chair: { type: String },
-  members: { type: Array },
-  transactions: { type: Array },
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  country: { type: String, required: true },
+  currency: { type: String, default: 'NGN' },
+  chair: {
+    name: { type: String },
+  },
+  members: [{
+    name: { type: String },
+  }],
+  transactions: [
+    {
+      txids: {
+        coinbase: { type: String },
+        bitpesa: { type: String },
+      },
+      date: { type: Date, default: Date.now },
+      ammount: { type: Number },
+      verified: { type: Boolean, default: false },
+    },
+  ],
 });
 
 const Bank = mongoose.model('Bank', bankSchema);
