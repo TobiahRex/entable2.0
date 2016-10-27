428d71c0f4a52cfa71c9fecddfe3fabf4deffee9
TobiahRex <tobiah.rex@gmail.com>
2016-10-27 01:39:27 -0700

    added bank create route.

--- server/api/banks.js
+++ server/api/banks.js
@@ -4,6 +4,7 @@ import Bank from '../models/Bank';
 const router = new express.Router();
 
 router.route('/')
-.get((req, res) => Bank.find({}, res.handle));
+.get((req, res) => Bank.find({}, res.handle))
+.post((req, res) => Bank.create(req.body, res.handle));
 
 export default router;
