6e2ea8cfb99909442321909b2dc1c5b061db6329
TobiahRex <tobiah.rex@gmail.com>
2016-10-27 01:00:46 -0700

    reformatted Twilio.js

--- server/models/Twilio.js
+++ server/models/Twilio.js
@@ -1,29 +1,5 @@
 import twilio from 'twilio';
 
-// const accountSid = process.env.TWILLIO_TEST_SID;
-// const authToken = process.env.TWILIO_TEST_AUTH_TOKEN;
-// const client = twilio(accountSid, authToken);
-// console.log('client: ', client);
-//
-// client.messages.create({
-//   to: '+14153210002',
-//   from: process.env.TWILIO_TEST_PHONE,
-//   body: 'Thank you for your donation. It has been received.',
-// }, (err, message) => {
-//   if (err) console.log('>>> ERROR: \n', err);
-//   console.log('>>> SUCCESS: \n', message);
-// });
-
-// const app = express();
-//
-// app.post('/sms', (req, res) => {
-//   const twiml = new twilio.TwimlResponse();
-//   twiml.message('Thanks for your donation. It has been received.');
-//   res.writeHead(200, { 'Content-Type': 'text/html' }).end(twiml.toString());
-// });
-//
-// http.createServer(app).listen(8000, (err) => console.log(err || 'Server @ 8000'));
-
 export const replyGreeting = (cb) => {
   const twiml = new twilio.TwimlResponse();
   twiml.message('Cheers from Entable!\n We appreciate your interest in helping the people of Africa. \n\nIf you would like to make a donation, text "#donate<ammount>" to us. And we will send you more information on how you can participate.\n Thanks again!');
