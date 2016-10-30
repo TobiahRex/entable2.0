f6da9c21920a6c510f070ba61bca8fa0277808ac
TobiahRex <tobiah.rex@gmail.com>
2016-10-30 03:07:59 -0700

    modified server folder structure

--- server/api/index.js
+++ server/api/index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import twilio from './twilio';
 import coinbase from './coinbase';
 import stripe from './stripe';
-import stripeWebhooks from './stripeWebhooks';
+import stripeEvent from './stripeEvent';
 
 const router = new express.Router();
 
 router.use('/twilio', twilio);
 router.use('/coinbase', coinbase);
 router.use('/stripe', stripe);
-router.use('/stripe/webhooks', stripeWebhooks);
+router.use('/stripe/webhooks', stripeEvent);
 
 export default router;
--- server/models/Stripe/stripeEvents.js
+++ server/models/Stripe/stripeEvents.js
@@ -1,9 +1,16 @@
+import Promise from 'bluebird';
 import stripeNode from 'stripe';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+const stripeEventSchema = new mongoose.Schema({
+  events: [{ type: Object }],
+});
 
 dotenv.config({ silent: true });
 
 const stripe = stripeNode(process.env.STRIPE_LIVE_SECRET_KEY);
+Promise.promisify(stripe);
 
 export const txfrToBank = amount =>
 stripe.transfer.create({
@@ -22,3 +29,16 @@ stripe.transfer.create({
 }, {
   stripe_account: process.env.STRIPE_DEFAULT_DEBIT_CARD_ACCT_ID,
 });
+
+export const saveEvent = eventObj => {
+  stripeAccount.findById();
+};
+
+export const saveAndVerifyEvent = (reqBody, cb) => {
+  stripe.events.retrieve(reqBody.id)
+  .then(event => saveEvent(event))
+  .then(() => cb(null, null))
+  .catch(err => cb(err));
+};
+
+const StripeEvent = mongoose.model('StripeEvent', stripeEventSchema);
