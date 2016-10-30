27ee9fb5757ca867e70ce8b08805610fd09f3adf
TobiahRex <tobiah.rex@gmail.com>
2016-10-30 01:35:26 -0700

    modified .env file in local project directory.

--- server/models/Stripe.js
+++ server/models/Stripe.js
@@ -1,3 +1,24 @@
 import stripeNode from 'stripe';
+import dotenv from 'dotenv';
 
-const stripe = stripeNode(process.env.STRIPE_API_SECRET)
+dotenv.config({ silent: true });
+
+const stripe = stripeNode(process.env.STRIPE_LIVE_SECRET_KEY);
+
+export const txfrToBank = amount =>
+stripe.transfer.create({
+  amount,
+  currency: 'usd',
+  destination: 'default_for_currency',
+}, {
+  stripe_account: process.env.STRIPE_DEFAULT_BANK_ACCT_ID,
+});
+
+export const txfrToDebitCard = amount =>
+stripe.transfer.create({
+  amount,
+  currency: 'usd',
+  method: 'instant',
+}, {
+  stripe_account: process.env.STRIPE_DEFAULT_DEBIT_CARD_ACCT_ID,
+});
