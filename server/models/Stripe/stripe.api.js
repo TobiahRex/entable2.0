562ef2957e9bfa7b90f62d9b658278727e853aac
TobiahRex <tobiah.rex@gmail.com>
2016-10-30 18:10:34 -0700

    created rxDonation method for saving and charging donation information.

--- server/models/Stripe/stripe.api.js
+++ server/models/Stripe/stripe.api.js
@@ -1,5 +1,22 @@
 import stripeNode from 'stripe';
 import dotenv from 'dotenv';
+import StripeAccount from './stripes';
 
 dotenv.config({ silent: true });
 const stripe = stripeNode(process.env.STRIPE_LIVE_SECRET_KEY);
+
+const acceptDonation = (token, donationInfo) =>
+Promise.fromCallback(cb => stripe.charges.create({
+  amount: donationInfo.amount,
+  currency: donationInfo.currency,
+  source: token,
+  description: 'Entable Donation',
+}, cb))
+
+export const rxDonation = (token, donationInfo, cb) => {
+  StripeAccount.saveDonationInfo(donationInfo)
+  .then(savedInfo => acceptDonation(token, savedInfo))
+  .then(charge => StripeAccount.savedChargeInfo(charge))
+  .then(savedCharge => cb(null, savedCharge))
+  .catch(error => cb({ ERROR_DONATION: 'There was an error processing your donation.', error }));
+};
