432e674e10c6d1280b04e66d4f13373dd3339c78
TobiahRex <tobiah.rex@gmail.com>
2016-11-01 22:28:42 -0700

    created GetPrices.js

--- src/Sagas/BankSaga/GetPrice.js
+++ src/Sagas/BankSaga/GetPrice.js
@@ -1,1 +1,13 @@
 import { call, put } from 'redux-saga';
+import BankActions from '../../Redux/BankRedux';
+import apiActions from '../../Redux/ApiRedux';
+
+export default function* create(api, action) {
+  const response = yield call(() => api.getExchangeRate(action.pair));
+
+  if (response.ok) {
+    yield [put(BankSActions.getExchangeRateSuccess(response.data))];
+  } else {
+    yield put(apiActions.apiFail(response.problem));
+  }
+}
