39227d7dd343bf44a1cbc3a47653c7719b131c5b
TobiahRex <tobiah.rex@gmail.com>
2016-11-03 23:07:44 -0700

    added comment breaks

--- server/api/banks.js
+++ server/api/banks.js
@@ -2,15 +2,20 @@ import express from 'express';
 import Bank from '../models/Bank';
 
 const router = new express.Router();
-
+// ----------------------------------------------------------------------------
 router.route('/')
 .get((req, res) => Bank.find({}, res.handle))
 .post((req, res) => Bank.create(req.body, res.handle));
-
+// ----------------------------------------------------------------------------
 router.route('/:id')
-.get((req, res) => Bank.findById(req.params.id, res.handle))
+.get((req, res) =>
+Bank.findById(req.params.id, res.handle))
+
 .put((req, res) =>
 Bank.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true }, res.handle))
-.delete((req, res) => Bank.findByIdAndRemove(req.params.id, res.handle));
+
+.delete((req, res) =>
+Bank.findByIdAndRemove(req.params.id, res.handle));
+// ----------------------------------------------------------------------------
 
 export default router;
