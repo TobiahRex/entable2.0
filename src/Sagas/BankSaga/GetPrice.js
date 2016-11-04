8bbc0bb245584af5171c139168cda2ec70ac71d7
TobiahRex <tobiah.rex@gmail.com>
2016-11-04 00:01:11 -0700

    created getBanks saga - UNTESTED.

--- src/Sagas/BankSaga/GetPrice.js
+++ src/Sagas/BankSaga/GetPrice.js
@@ -2,11 +2,12 @@ import { call, put } from 'redux-saga';
 import BankActions from '../../Redux/BankRedux';
 import apiActions from '../../Redux/ApiRedux';
 
-export default function* create(api, action) {
+export default function* (api, action) {
   const response = yield call(() => api.getExchangeRate(action.pair));
 
   if (response.ok) {
-    yield [put(BankActions.getExchangeRateSuccess(response.data))];
+    yield [put(BankActions.getExchangeRateSuccess(response.data)),
+    apiActions.apiSuccess()];
   } else {
     yield put(apiActions.apiFail(response.problem));
   }
---
+++ src/Sagas/BankSaga/getBanks.js
@@ -0,0 +1,14 @@
+import { call, put } from 'redux-saga';
+import BankActions from '../../Redux/BankRedux';
+import apiActions from '../../Services/API';
+
+export default function* (api) {
+  const response = yield call(api.getBanks);
+
+  if (response.ok) {
+    yield [put(BankActions.receivedBanks(response.data)),
+    put(apiActions.apiSuccess())];
+  } else {
+    yield put(apiActions.apiFail(response.data));
+  }
+}
