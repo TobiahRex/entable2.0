9f8066b2c60217d0fdeb13b85081410bf0e227a5
TobiahRex <tobiah.rex@gmail.com>
2016-11-04 16:38:57 -0700

    modified BankActivity.js

--- src/Components/BankActivity.js
+++ src/Components/BankActivity.js
@@ -1,5 +1,35 @@
 import React from 'react';
 
+/* Old Logic
+if (banks.length) {
+  rows = banks.map((bank, index) => {
+    const membersInfo = {
+      17148750963: 'Ziya Emanet',
+      19199601124: 'Donovan Moore',
+      19259973408: 'Richard Mands',
+      19253534139: 'Joshua Maddox',
+      14802745839: 'Holly Zhou',
+    };
+
+    let borrowed = 0;
+    let payedIn = 0;
+
+    // const { sender, date, amount, description } = bank;
+    const { description, finance } = bank;
+
+    if (finance.balance.current < 0) {
+      borrowed = finance.balance.current * (-1);
+    } else {
+      payedIn = finance.balance.current;
+    }
+
+    startingAmount = finance.balance.starting;
+
+  });
+} else {
+  rows = <tr />;
+}
+*/
 const BankActivity = ({ transactions }) => {
   const rows = transactions.map((transaction) => {
     return 'empty';
--- src/Containers/BanksContainer.js
+++ src/Containers/BanksContainer.js
@@ -35,43 +35,7 @@ class BankDetail extends Component {
 
     if (!amountNumber) amountNumber = 0;
 
-    if (banks.length) {
-      rows = banks.map((bank, index) => {
-        const membersInfo = {
-          17148750963: 'Ziya Emanet',
-          19199601124: 'Donovan Moore',
-          19259973408: 'Richard Mands',
-          19253534139: 'Joshua Maddox',
-          14802745839: 'Holly Zhou',
-        };
-
-        let borrowed = 0;
-        let payedIn = 0;
-
-        // const { sender, date, amount, description } = bank;
-        const { description, finance } = bank;
-
-        if (finance.balance.current < 0) {
-          borrowed = finance.balance.current * (-1);
-        } else {
-          payedIn = finance.balance.current;
-        }
-
-        startingAmount = finance.balance.starting;
-
-        return (
-          <tr key={index}>
-          {/* <td>{membersInfo[sender]}</td>
-          <td>{date}</td>
-          <td>{borrowed}</td>
-          <td>{payedIn}</td>
-          <td>{description}</td> */}
-          </tr>
-        );
-      });
-    } else {
-      rows = <tr />;
-    }
+    
 
     const banks = this.state.banks.map(bank =>
       <BankDisplay key={bank._id} bank={bank} />
