ac86cbc98954a64a01a2847976612b50e6b9e537
TobiahRex <tobiah.rex@gmail.com>
2016-11-04 17:53:01 -0700

    completed first draft of Banks.js

---
+++ src/Components/Donation/donateBank.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import moment from 'moment';
+import Footer from '../Footer';
+
+export default class Donation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // banks: BankStore.getBanks(),
+    };
+    this._onSubmit = this._onSubmit.bind(this);
+  }
+
+  _onSubmit(e) {
+    e.preventDefault();
+    const amount = this.refs.amount.value;
+    browserHistory.push({ pathname: '/bank', query: { amountNumber: amount } });
+  }
+
+  render() {
+    const mainBankImage = {
+      backgroundImage: 'url(enable-women-to-be-the-boss.jpg)',
+    };
+
+    const fakeTimeStampDelete = moment().format('lll');
+    const bankName = "Women's Bank of Anlos";
+    return (
+      <div>
+        <div className=" donationContainer">
+          <div className="bankImgBanner" style={mainBankImage}>
+            <div className="donationBankName">
+              <h2>{bankName}</h2>
+              <h5>
+                96% of your donation goes directly to the owners of this bank.
+                <br />
+                4% covers USD to Bitcoin to Mpesa transactions fees of third party money handlers.
+              </h5>
+            </div>
+            <form className="infoForm" onSubmit={this._onSubmit}>
+              <div className="form-group">
+                <label className="sr-only" htmlFor="exampleInputEmail3">Supporter Email</label>
+                <input type="email" className="form-control" id="exampleInputEmail3" defaultValue="sample@gmail.com" />
+              </div>
+              <div className="form-group">
+                <label className="sr-only" htmlFor="exampleInputPassword3">Bit Coin Address</label>
+                <input type="text" className="form-control" id="exampleInputPassword3" defaultValue="19Ms9tlqtcqAJ1ue36e9kjnyzkkLy18EQuBY" />
+              </div>
+              <div className="form-group">
+                <label className="sr-only" htmlFor="exampleInputPassword3">{('Supporter\'s')} Name</label>
+                <input type="text" className="form-control" id="exampleInputPassword3" defaultValue="Holly Zhou" />
+              </div>
+              <div className="form-group">
+                <label className="sr-only" htmlFor="exampleInputPassword3">Amount</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputPassword3"
+                  placeholder="200"
+                  ref="amount"
+                />
+              </div>
+              <button type="submit" className="btn btn-default btn-block btnFormBottom">Send Payment</button>
+            </form>
+          </div>
+          <div className="donationOptions">
+            <div className="giftBtnContainer">
+              <button className="giftBtn">Send As A Gift</button>
+            </div>
+            <div className="noBtnCoinsContainer">
+              <button className="noBTcoins">I {('Don\'t')} Have Bitcoin</button>
+            </div>
+          </div>
+          <div className="transactionHeaderDonation">
+            <h3>Bank History & Notes to Sponsors</h3>
+          </div>
+          <div className="transactionHistoryContainer text-center">
+            <div className="transactionItem">
+              <h6>{String('')}</h6>
+              <p><span className="timeStamp">{fakeTimeStampDelete}</span> <span className="bankerName">  Betty Hascal - <i>Bank Manager</i></span></p>
+              <p>{('Sarah\'s')} son started school with new books.</p>
+              <hr className="bankHistoryHr" />
+            </div>
+            <div className="transactionItem">
+              <h6>{String('')}</h6>
+              <p><span className="timeStamp">{fakeTimeStampDelete}</span> <span className="bankerName">  Betty Hascal - <i>Bank Manager</i></span></p>
+              <p>{('Anile\'s')} farm bought breeding pigs.</p>
+              <hr className="bankHistoryHr" />
+            </div>
+            <div className="transactionItem">
+              <h6>{String('')}</h6>
+              <p><span className="timeStamp">{fakeTimeStampDelete}</span> <span className="bankerName">  Betty Hascal - <i>Bank Manager</i></span></p>
+              <p>Aide bought supplies to increase productivity of chicken operation.</p>
+              <hr className="bankHistoryHr" />
+            </div>
+            <div className="transactionItem">
+              <h6>{String('')}</h6>
+              <p><span className="timeStamp">{fakeTimeStampDelete}</span> <span className="bankerName">  Betty Hascal - <i>Bank Manager</i></span></p>
+              <p>{('Sarah\'s')} son started school with new books.</p>
+              <hr className="bankHistoryHr" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Banks.js
+++ src/components/Banks.js
@@ -1,16 +1,5 @@
 import React, { Component } from 'react';
-import { browserHistory } from 'react-router';
-import bankData from './BankDetails/bankData/index';
 import BankCard from '../Components/BankCard';
-
-const toBank = () => browserHistory.push('bank');
-const toBank2 = () => browserHistory.push('bank2');
-const toBank3 = () => browserHistory.push('bank3');
-const toBank4 = () => browserHistory.push('bank4');
-const toBank5 = () => browserHistory.push('bank5');
-const toBank6 = () => browserHistory.push('bank6');
-const toFund = () => browserHistory.push('donation');
-
 
 export default class Banks extends Component {
   constructor() {
@@ -37,98 +26,21 @@ export default class Banks extends Component {
     return [];
   }
 
-  render() {
-    const dummy = bankData.data;
-    // const { banks } = this.state;
-    return (
-      <div className="banksContainer">
-        <div className="bankCallToAction">
-          <h4>Entable is an SMS (Tropo) powered table banking platform
-            that pairs under-served women without access to financial
-          services to sponsors seeking complete transparency and direct impact.</h4>
-        </div>
+  render = () => (
+    <div className="banksContainer">
+      <div className="bankCallToAction">
+        <h4>Entable is an SMS (Tropo) powered table banking platform
+          that pairs under-served women without access to financial
+        services to sponsors seeking complete transparency and direct impact.</h4>
+      </div>
 
-        <div className="row bankRows">
-          <div className="container">
-
-            <div className="col-md-4 text-center bkMargin">
-              <div className="bankSingleDisplay panel panel-default">
-                <img className="bankImg" role="presentation" src={dummy[5].Images[0]} width="100%" height="80%" />
-                <div className="imgTxtContainer" />
-                <div className="bankViewBtnDisplay">
-                  <button className="bankViewBtn" onClick={toFund}>Fund Bank</button>
-                  <button className="bankViewBtn" onClick={toBank}>View Bank</button>
-                </div>
-                <p className="bankViewDesc">
-                  {dummy[5].Description1}
-                  {dummy[5].Description2}
-                </p>
-              </div>
-            </div>
-
-            <div className="col-md-4 text-center bkMargin">
-              <div className="bankSingleDisplay panel panel-default">
-                <img className="bankImg" role="presentation" src={dummy[0].Images[0]} width="100%" height="80%" />
-                <div className="imgTxtContainer" />
-                <div className="bankViewBtnDisplay">
-                  <button className="bankViewBtn" onClick={toFund}>Fund Bank</button>
-                  <button className="bankViewBtn" onClick={toBank2}>View Bank</button>
-                </div>
-                <p className="bankViewDesc">{dummy[0].Description1} {dummy[0].Description2}</p>
-              </div>
-            </div>
-
-            <div className="col-md-4 text-center bkMargin">
-              <div className="bankSingleDisplay panel panel-default">
-                <img className="bankImg" role="presentation" src={dummy[1].Images[0]} width="100%" height="80%" />
-                <div className="imgTxtContainer" />
-                <div className="bankViewBtnDisplay">
-                  <button className="bankViewBtn" onClick={toFund}>Fund Bank</button>
-                  <button className="bankViewBtn" onClick={toBank3}>View Bank</button>
-                </div>
-                <p className="bankViewDesc">{dummy[1].Description1} {dummy[1].Description2}</p>
-              </div>
-            </div>
-
-            <div className="col-md-4 text-center bkMargin">
-              <div className="bankSingleDisplay panel panel-default">
-                <img className="bankImg" src={dummy[2].Images[0]} role="presentation" width="100%" height="80%" />
-                <div className="imgTxtContainer" />
-                <div className="bankViewBtnDisplay">
-                  <button className="bankViewBtn" onClick={toFund}>Fund Bank</button>
-                  <button className="bankViewBtn" onClick={toBank4}>View Bank</button>
-                </div>
-                <p className="bankViewDesc">{dummy[2].Description1} {dummy[2].Description2}</p>
-              </div>
-            </div>
-
-            <div className="col-md-4 text-center bkMargin">
-              <div className="bankSingleDisplay panel panel-default">
-                <img className="bankImg" src={dummy[3].Images[0]} role="presentation" width="100%" height="80%" />
-                <div className="imgTxtContainer" />
-                <div className="bankViewBtnDisplay">
-                  <button className="bankViewBtn" onClick={toFund}>Fund Bank</button>
-                  <button className="bankViewBtn" onClick={toBank5}>View Bank</button>
-                </div>
-                <p className="bankViewDesc">{dummy[3].Description1} {dummy[3].Description2}</p>
-              </div>
-            </div>
-
-            <div className="col-md-4 text-center bkMargin">
-              <div className="bankSingleDisplay panel panel-default">
-                <img className="bankImg" src={dummy[4].Images[0]} role="presentation" width="100%" height="80%" />
-                <div className="imgTxtContainer" />
-                <div className="bankViewBtnDisplay">
-                  <button className="bankViewBtn" onClick={toFund}>Fund Bank</button>
-                  <button className="bankViewBtn" onClick={toBank6}>View Bank</button>
-                </div>
-                <p className="bankViewDesc">{dummy[4].Description1} {dummy[4].Description2}</p>
-              </div>
-            </div>
-
-          </div>
+      <div className="row bankRows">
+        <div className="container">
+          {/* This component function creates a list of <BankCard />
+          components.  Observe render function for more details. */}
+          {this.renderBankCards()}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
