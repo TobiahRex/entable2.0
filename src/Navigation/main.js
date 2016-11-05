959fcaa3efb6160c57efcb6405e174f9a8801e7a
TobiahRex <tobiah.rex@gmail.com>
2016-11-04 17:49:02 -0700

    modified main router.

--- src/Navigation/main.js
+++ src/Navigation/main.js
@@ -5,20 +5,13 @@ import Navbar from '../Components/Navbar';
 import Homepage from '../Containers/Home';
 import BankDetail from '../Components/BankDetails/BankDetail';
 import donateBank from '../Components/Donation/donateBank';
-import donateGeneral from '../Components/Donation/donateBank';
+import donateGen from '../Components/Donation/donateGen';
 
 export default (
   <Route path="/" component={Navbar}>
     <IndexRoute component={Homepage} />
     <Router path="bank/:id" component={BankDetail} />
-    <Router path="donation" component={donateGeneral} />
+    <Router path="donation" component={donateGen} />
     <Router path="donation/:id" component={donateBank} />
-    {/* <Router path="bank2" component={BankDetail2} />
-      <Router path="bank3" component={BankDetail3} />
-      <Router path="bank4" component={BankDetail4} />
-      <Router path="bank5" component={BankDetail5} />
-      <Router path="bank6" component={BankDetail6} />
-      <Router path="bank7" component={BankDetail7} />
-    */}
-    </Route>
+  </Route>
 );
