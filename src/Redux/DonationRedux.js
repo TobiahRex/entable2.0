bf21556017319ba85e83606cecf8e55143ce3141
TobiahRex <tobiah.rex@gmail.com>
2016-11-06 14:19:55 -0800

    created donationReducer.

--- src/Redux/DonationRedux.js
+++ src/Redux/DonationRedux.js
@@ -2,18 +2,37 @@ import { createActions, createReducer } from 'reduxsauce';
 
 const { Types, Creators } = createActions({
   sendDonation: null,
-  donationSuccess: null,
-  donationFail: null,
+  donationSuccess: ['amount'],
+  donationFail: ['response'],
 });
 
 export const donationTypes = Types;
 export default Creators;
 
 export default INITIAL_STATE = {
+  amount: 0,
   sent: null,
   success: null,
   fail: null,
-  amount: 0,
+  response: null,
 }
 
-const success = (state, action) =>
+const success = (state, { amount }) => ({
+  amount,
+  sent: true,
+  fail: false,
+  success: true,
+  response: null,
+});
+
+const fail = (state, { response }) => ({
+  amount: 0,
+  sent: true,
+  fail: true,
+  success: false,
+});
+
+export default donationReducer = createReducer(INITIAL_STATE, {
+  [Types.DONATION_SUCCESS]: success,
+  [Types.DONATION_FAIL]: fail,
+});
