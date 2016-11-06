8a996cd497f5814c0d46ba4e37c5fb47ef4e7d86
TobiahRex <tobiah.rex@gmail.com>
2016-11-05 19:53:53 -0700

    added twilio to state.

--- src/Sagas/Twilio/sendText.js
+++ src/Sagas/Twilio/sendText.js
@@ -1,5 +1,15 @@
 import { call, put } from 'redux-saga/effects';
-import TwilioActions from '../../Redux/TwilioRedux';
+import twilioActions from '../../Redux/TwilioRedux';
 import apiActions from '../../Redux/ApiRedux';
 
-export default function* sendText
+export default function* sendText(api, { text }) {
+  const response = yield call(() => api.sendText(text));
+
+  if (response.ok) {
+    yield [put(twilioActions.textSuccess(response.data)),
+    put(apiActions.apiSuccess())];
+  } else {
+    yield [put(twilioActions.textFail(response.data)),
+    put(apiActions.apiFail(response.data))];
+  }
+}
