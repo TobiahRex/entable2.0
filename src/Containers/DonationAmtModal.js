2671e8d52d8ee00dbdbf08e5c29805b49a4139b9
TobiahRex <tobiah.rex@gmail.com>
2016-11-17 18:24:57 -0800

    imported react-bootstrap Modal.

--- src/Containers/DonationAmtModal.js
+++ src/Containers/DonationAmtModal.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import Modal from 'react-bootstrap/lib/Modal';
 
 class AmtModal extends React.Component {
   constructor(props) {
@@ -15,23 +16,24 @@ class AmtModal extends React.Component {
 
   render() {
     return (
-      <div className="modal fade" tabIndex="-1" role="dialog">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-              <h4 className="modal-title">Modal title</h4>
-            </div>
-            <div className="modal-body">
-              <p>One fine body&hellip;</p>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Modal show={this.props.showModal} onHide={this.close}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Text in a modal</h4>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+          <h4>Popover in a modal</h4>
+          <hr />
+
+          <h4>Overflowing text to show scroll behavior</h4>
+          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button onClick={this.close}>Close</button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
