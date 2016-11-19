451a26164bd8f8f70b035a417442addd9d258acf
TobiahRex <tobiah.rex@gmail.com>
2016-11-18 18:21:52 -0800

    adding additional fields to Register component.

--- src/Containers/Register.js
+++ src/Containers/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Breadcrumbs from '../Components/Breadcrumb';
 
 class Register extends React.Component {
   static propTypes = {
@@ -8,13 +9,86 @@ class Register extends React.Component {
     super(props);
 
     this.state = {
-      username: '',
+      firstName: '',
+    };
+    this.breadCrumbs = [{
+      href: '/',
+      name: 'Home',
+      active: false,
+    }, {
+      href: '/register',
+      name: 'Register',
+      active: true,
+    }];
+    this.styles = {
+      registerTitle: {
+        padding: '0px 140px 0px 140px',
+      },
     };
   }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('nextState: ', nextState);
+    return true;
+  }
+
+  onInputChange = (value, id) => this.setState({ [id]: value })
+
   render() {
     return (
       <div>
-        <h1>Register as a Donor</h1>
+        <Breadcrumbs paths={this.breadCrumbs} />
+        <h1 style={this.styles.registerTitle}>Register as a Donor</h1>
+        <hr />
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          onChange={e =>
+            this.onInputChange(e.target.value, e.target.getAttribute('id'))}
+        />
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          onChange={e =>
+            this.onInputChange(e.target.value, e.target.getAttribute('id'))}
+        />
+        <label htmlFor="emailAddress">Email Address</label>
+        <input
+          type="text"
+          id="emailAddress"
+          onChange={e =>
+            this.onInputChange(e.target.value, e.target.getAttribute('id'))}
+        />
+        <label htmlFor="dateOfBirth">Date of Birth</label>
+        <div>
+          <input
+            type="text"
+            id="dateOfBirth"
+            onChange={e =>
+              this.onInputChange(e.target.value, e.target.getAttribute('id'))}
+          />
+        </div>
+        <label htmlFor="streetAddress">Street Address</label>
+        <div>
+          <input
+            type="text"
+            id="streetAddress"
+            onChange={e =>
+              this.onInputChange(e.target.value, e.target.getAttribute('id'))}
+          />
+        </div>
+        <label htmlFor="addressLine2">Address Line 2</label>
+        <div>
+          <input
+            type="text"
+            id="addressLine2"
+            onChange={e =>
+              this.onInputChange(e.target.value, e.target.getAttribute('id'))}
+          />
+        </div>
+        bk
       </div>
     );
   }
