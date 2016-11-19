68115242068e3703f31ab2ba076a1b8525a3d919
TobiahRex <tobiah.rex@gmail.com>
2016-11-18 18:42:54 -0800

    added register component styling and spacing.

--- src/Containers/Register.js
+++ src/Containers/Register.js
@@ -26,8 +26,14 @@ class Register extends React.Component {
         padding: '0px 140px 0px 140px',
       },
       inputContainers: {
-        padding: '50spx 140px 50px 140px',
+        padding: '25px 140px 50px 140px',
       },
+      userInfoDiv: {
+        width: '270px',
+      },
+      userInfoInput: {
+        width: '250px',
+      }
     };
   }
 
@@ -45,63 +51,70 @@ class Register extends React.Component {
         <h1 style={this.styles.registerTitle}>Register as a Donor</h1>
         <hr />
         <div style={this.styles.inputContainers}>
-          <label htmlFor="firstName">First Name</label>
-          <div>
+          <div style={this.styles.userInfoInput}>
+            <label htmlFor="firstName">First Name</label>
             <input
+              style={this.styles.userInfoInput}e={this.styles.userInfoInput}
               type="text"
               id="firstName"
               onChange={e =>
                 this.onInputChange(e.target.value, e.target.getAttribute('id'))}
             />
           </div>
-          <label htmlFor="lastName">Last Name</label>
-          <div>
+          <div style={this.styles.userInfoInput}>
+            <label htmlFor="lastName">Last Name</label>
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="lastName"
               onChange={e =>
                 this.onInputChange(e.target.value, e.target.getAttribute('id'))}
             />
           </div>
-          <label htmlFor="emailAddress">Email Address</label>
-          <div>
+          <div style={this.styles.userInfoInput}>
+            <label htmlFor="emailAddress">Email Address</label>
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="emailAddress"
               onChange={e =>
                 this.onInputChange(e.target.value, e.target.getAttribute('id'))}
             />
           </div>
-          <label htmlFor="dateOfBirth">Date of Birth</label>
-          <div>
+          <div style={this.styles.userInfoInput}>
+            <label htmlFor="dateOfBirth">Date of Birth</label>
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="dateOfBirth"
               onChange={e =>
                 this.onInputChange(e.target.value, e.target.getAttribute('id'))}
             />
           </div>
-          <label htmlFor="streetAddress">Street Address</label>
-          <div>
+          <div style={this.styles.userInfoInput}>
+            <label htmlFor="streetAddress">Street Address</label>
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="streetAddress"
               onChange={e =>
                 this.onInputChange(e.target.value, e.target.getAttribute('id'))}
             />
           </div>
-          <label htmlFor="addressLine2">Address Line 2</label>
-          <div>
+          <div style={this.styles.userInfoInput}>
+            <label htmlFor="addressLine2">Address Line 2</label>
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="addressLine2"
               onChange={e =>
                 this.onInputChange(e.target.value, e.target.getAttribute('id'))}
             />
           </div>
-          <label htmlFor="city">City</label>
-          <div>
+          <div style={this.styles.userInfoInput}>
+            <label htmlFor="city">City</label>
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="city"
               onChange={e =>
@@ -109,26 +122,29 @@ class Register extends React.Component {
             />
           </div>
           <label htmlFor="stateProvince">State / Province</label>
-          <div>
+          <div style={this.styles.userInfoInput}          >
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="stateProvince"
               onChange={e =>
                 this.onInputChange(e.target.value, e.target.getAttribute('id'))}
             />
           </div>
-          <label htmlFor="postZipCode">Post / Zip Code</label>
-          <div>
+          <div style={this.styles.userInfoInput}>
+            <label htmlFor="postZipCode">Post / Zip Code</label>
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="postZipCode"
               onChange={e =>
                 this.onInputChange(e.target.value, e.target.getAttribute('id'))}
             />
           </div>
+          <div style={this.styles.userInfoInput}>
           <label htmlFor="country">Country</label>
-          <div>
             <input
+              style={this.styles.userInfoInput}
               type="text"
               id="country"
               onChange={e =>
