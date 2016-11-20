600b4d7a109859e81a9b21363719decf8fcdf098
TobiahRex <tobiah.rex@gmail.com>
2016-11-20 14:59:17 -0800

    added Breadcrumbs to BankExmaple.js

---
+++ src/Containers/Register/registerStyles.js
@@ -0,0 +1,44 @@
+const styles = {
+  registerFlexParent: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  registerTitle: {
+    padding: '0px 140px 0px 140px',
+  },
+  registerH4: {
+    textAlign: 'center',
+  },
+  registerPrivacy: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  registerPrivacyContainer: {
+    alignSelf: 'center',
+  },
+  registerPrivacyMsg: {
+    width: '80%',
+    margin: '0px 100px',
+  },
+  inputContainers: {
+    padding: '25px 140px 50px 140px',
+  },
+  userInfoDiv: {
+    width: '270px',
+  },
+  userInfoInput: {
+    width: '250px',
+  },
+  privacyLink: {
+    color: '#2ecc71',
+    textShadow: '0 1px 3px rgba(210, 5, 5, 0.12), 0 1px 2px rgba(249, 249, 249, 0.24)',
+  },
+  required: {
+    color: 'red',
+  },
+  centerText: {
+    textAlign: 'center',
+  },
+};
+export default styles;
