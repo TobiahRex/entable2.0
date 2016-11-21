53e5ad38951f0b8ff0325ba5b70c0351da82d2c0
TobiahRex <tobiah.rex@gmail.com>
2016-11-21 15:27:27 -0800

    completed creating loginDetails.register.js component - Need to pass down props from registerContainer now.

--- src/Containers/Register/loginDetails.register.js
+++ src/Containers/Register/loginDetails.register.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import Inputcard from '../../Components/Register/InputCard';
+import React, { PropTypes } from 'react';
+import Inputcard from '../../Components/InputCard';
+import styles from './registerStyles';
 
 class loginDetails extends React.PureComponent {
+  static propTypes = {
+    password: PropTypes.objectOf(PropTypes.string),
+    confirmPassword: PropTypes.objectOf(PropTypes.string),
+  }
   render() {
     return (
       <span>
-
-        <h4 style={Register.styles.registerH4}>Login Details</h4>
-        <div style={Register.styles.inputContainers}>
+        <h4 style={styles.registerH4}>Login Details</h4>
+        <div style={styles.inputContainers}>
           <Inputcard
-            {...Register.PROPS.password}
+            {...this.props.password}
             value={this.state.password}
             onInputChange={this.onInputChange}
             validate={this.validate}
           />
           <br />
           <Inputcard
-            {...Register.PROPS.confirmPassword}
+            {...this.props.confirmPassword}
             value={this.state.confirmPassword}
             onInputChange={this.onInputChange}
             validate={this.validate}
           />
         </div>
       </span>
-    )
+    );
   }
 }
+export default loginDetails;
