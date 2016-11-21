c0c23263763ee2a35cf46d513f9dd69fd6c3b61d
TobiahRex <tobiah.rex@gmail.com>
2016-11-21 15:07:53 -0800

    created a hybrid approach for rendering static props.

--- src/Containers/Register/Register.js
+++ src/Containers/Register/Register.js
@@ -38,6 +38,72 @@ class Register extends React.Component {
       name: 'Register',
       active: true,
     }];
+    this.PROPS = {
+      firstName: {
+        id: 'firstName',
+        type: 'text',
+        name: 'First Name',
+        required: true,
+        vSuccess: 1,
+        vWarn: 1,
+        vError: 2,
+      },
+      lastName: {
+        id: 'lastName',
+        type: 'text',
+        name: 'Last Name',
+        required: true,
+        vSuccess: 1,
+        vWarn: 1,
+        vError: 2,
+      },
+      email: {
+        id: 'email',
+        type: 'email',
+        name: 'Email Address',
+        required: true,
+      },
+      postZip: {
+        id: 'postZip',
+        type: 'text',
+        name: 'Post / Zip Code',
+        required: true,
+        vSuccess: 4,
+        vWarn: 1,
+        vError: 0,
+      },
+      phone: {
+        id: 'phone',
+        name: 'Phone Number',
+        required: true,
+        requiredMsg: ' 123-456-7890',
+        vSuccess: 11,
+        vWarning: 10,
+        vError: 1,
+      },
+      password: {
+        id: 'password',
+        type: 'password',
+        name: 'Password',
+        required: true,
+        requiredMsg: ' At least 8 characters',
+        value: this.state.password,
+        onInputChange: this.onInputChange,
+        validate: this.validate,
+        vSuccess: 7,
+        vWarning: 2,
+        vError: 1,
+      },
+      confirmPassword: {
+        id: 'confirmPassword',
+        type: 'password',
+        name: 'Confirm Password',
+        required: true,
+        value: this.state.confirmPassword,
+        onInputChange: this.onInputChange,
+        validate: this.validate,
+      },
+    }
     this.countries = Countries.map((country, i) => (
       <MenuItem
         key={`country${i}`}
@@ -100,112 +166,7 @@ class Register extends React.Component {
     }
   }
 
-  // renderCountries = () => {
-  //   return Countries.map((country, i) => (
-  //     <MenuItem
-  //       key={`country${i}`}
-  //       eventKey={i + 1}
-  //       onClick={() => this.setState({ country })}
-  //     >
-  //       {country.name} - ({country.code})
-  //     </MenuItem>)
-  //   );
-  // }
-
   render() {
-    console.log('this.state.country: ', this.state.country);
-    const PROPS = {
-      firstName: {
-        id: 'firstName',
-        type: 'text',
-        name: 'First Name',
-        required: true,
-        value: this.state.firstName,
-        onInputChange: this.onInputChange,
-        validate: this.validate,
-        vSuccess: 1,
-        vWarn: 1,
-        vError: 2,
-      },
-      lastName: {
-        id: 'lastName',
-        type: 'text',
-        name: 'Last Name',
-        required: true,
-        value: this.state.lastName,
-        onInputChange: this.onInputChange,
-        validate: this.validate,
-        vSuccess: 1,
-        vWarn: 1,
-        vError: 2,
-      },
-      email: {
-        id: 'email',
-        type: 'email',
-        name: 'Email Address',
-        required: true,
-        value: this.state.email,
-        onInputChange: this.onInputChange,
-        validate: this.validate,
-      },
-      postZip: {
-        id: 'postZip',
-        type: 'number',
-        name: 'Post / Zip Code',
-        required: true,
-        value: this.state.postZip,
-        onInputChange: this.onInputChange,
-        validate: this.validate,
-        vSuccess: 4,
-        vWarn: 1,
-        vError: 0,
-      },
-      country: {
-        id: 'country',
-        name: 'Country',
-        required: true,
-        value: this.state.country,
-        onInputChange: this.onInputChange,
-        validate: this.validate,
-        vSuccess: 2,
-        vWarn: 3,
-        vError: 1,
-      },
-      phone: {
-        id: 'phone',
-        name: 'Phone Number',
-        required: true,
-        requiredMsg: ' 123-456-7890',
-        value: this.state.phone,
-        onInputChange: this.onInputChange,
-        validate: this.validate,
-        vSuccess: 11,
-        vWarning: 10,
-        vError: 1,
-      },
-      password: {
-        id: 'password',
-        type: 'password',
-        name: 'Password',
-        required: true,
-        requiredMsg: ' At least 8 characters',
-        value: this.state.password,
-        onInputChange: this.onInputChange,
-        validate: this.validate,
-        vSuccess: 7,
-        vWarning: 2,
-        vError: 1,
-      },
-      confirmPassword: {
-        id: 'confirmPassword',
-        type: 'password',
-        name: 'Confirm Password',
-        required: true,
-        value: this.state.confirmPassword,
-        onInputChange: this.onInputChange,
-        validate: this.validate,
-      },
-    };
     return (
       <div style={this.styles.mainBgColor}>
         <Breadcrumbs paths={this.breadCrumbs} />
@@ -219,13 +180,33 @@ class Register extends React.Component {
           <div>
             <h4 style={this.styles.registerH4}>Personal Details</h4>
             <div style={this.styles.inputContainers}>
-              <Inputcard {...PROPS.firstName} />
+              <Inputcard
+                {...this.PROPS.firstName}
+                value={this.state.firstName}
+                onInputChange={this.onInputChange}
+                validate={this.validate}
+              />
               <br />
-              <Inputcard {...PROPS.lastName} />
+              <Inputcard
+                {...this.PROPS.lastName}
+                value={this.state.lastName}
+                onInputChange={this.onInputChange}
+                validate={this.validate}
+              />
               <br />
-              <Inputcard {...PROPS.email} />
+              <Inputcard
+                {...this.PROPS.email}
+                value={this.state.email}
+                onInputChange={this.onInputChange}
+                validate={this.validate}
+              />
               <br />
-              <Inputcard {...PROPS.postZip} />
+              <Inputcard
+                {...this.PROPS.postZip}
+                value={this.state.postZip}
+                onInputChange={this.onInputChange}
+                validate={this.validate}
+              />
               <br />
               <div>
                 <label htmlFor="country">Country:
@@ -242,7 +223,12 @@ class Register extends React.Component {
           <div className="registerGroup">
             <h4 style={this.styles.registerH4}>Additional Details</h4>
             <div style={this.styles.additionalContainer}>
-              <Inputcard {...PROPS.phone} />
+              <Inputcard
+                {...this.PROPS.phone}
+                value={this.state.phone}
+                onInputChange={this.onInputChange}
+                validate={this.validate}
+              />
               <br />
               <div style={this.styles.userInfoInput}>
                 <label htmlFor="role">Entable Role:
@@ -265,9 +251,9 @@ class Register extends React.Component {
             <br />
             <h4 style={this.styles.registerH4}>Login Details</h4>
             <div style={this.styles.inputContainers}>
-              <Inputcard {...PROPS.password} />
+              <Inputcard {...this.PROPS.password} />
               <br />
-              <Inputcard {...PROPS.confirmPassword} />
+              <Inputcard {...this.PROPS.confirmPassword} />
             </div>
           </div>
         </div>
