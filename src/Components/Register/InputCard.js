59d02558f51467c86da54ff96cd5776b968375d3
TobiahRex <tobiah.rex@gmail.com>
2016-11-20 16:25:31 -0800

    installed reselect.

--- src/Components/Register/InputCard.js
+++ src/Components/Register/InputCard.js
@@ -1,7 +1,7 @@
 import React, { PropTypes, PureComponent } from 'react';
 import styles from '../../Containers/Register/registerStyles';
 
-class InputCard extends React.PureComponent {
+class InputCard extends PureComponent {
 
   render() {
     return (
@@ -14,14 +14,16 @@ class InputCard extends React.PureComponent {
           className="reg-form-control"
           type="text"
           id="firstName"
+          value={this.props.firstName}
           onChange={e =>
             this.onInputChange(e.target.value, e.target.getAttribute('id'))}
         />
       </div>
-      );
+    );
   }
 };
 InputCard.propTypes = {
+  firstName: PropTypes.string,
   onInputChange: PropTypes.func.isRequired,
 };
 
