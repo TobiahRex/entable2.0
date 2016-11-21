3c7d246244a8d4d45fe9882984a9f99dacaf332f
TobiahRex <tobiah.rex@gmail.com>
2016-11-20 16:43:14 -0800

    modified InputCard to reflect dynamic props and added "prefer-stateless-function" settings in .eslintrc to ignore React.PureComponent.

--- src/Components/Register/InputCard.js
+++ src/Components/Register/InputCard.js
@@ -2,28 +2,30 @@ import React, { PropTypes, PureComponent } from 'react';
 import styles from '../../Containers/Register/registerStyles';
 
 class InputCard extends PureComponent {
-
   render() {
     return (
       <div style={styles.userInfoInput}>
-        <label htmlFor="firstName">First Name:
-          <span style={styles.required}> *</span>
+        <label htmlFor={this.props.id}>{this.props.name}:
+          {this.props.required ? <span style={styles.required}> *</span> : ''}
         </label>
         <input
           style={styles.userInfoInput}
           className="reg-form-control"
           type="text"
-          id="firstName"
-          value={this.props.firstName}
+          id={this.props.id}
+          value={this.props.value}
           onChange={e =>
-            this.onInputChange(e.target.value, e.target.getAttribute('id'))}
+            this.props.onInputChange(e.target.value, e.target.getAttribute('id'))}
         />
       </div>
     );
   }
-};
+}
 InputCard.propTypes = {
-  firstName: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  required: PropTypes.bool.isRequired,
+  value: PropTypes.string,
   onInputChange: PropTypes.func.isRequired,
 };
 
