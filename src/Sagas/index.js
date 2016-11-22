1658ad09a8180602ecddab863b7b42abfff47997
TobiahRex <tobiah.rex@gmail.com>
2016-11-22 11:56:44 -0800

    imported firebaseAuth as firebase, imported registerUserSaga into rootSaga.

--- src/Sagas/index.js
+++ src/Sagas/index.js
@@ -1,13 +1,14 @@
 import { takeLatest } from 'redux-saga';
 import API from '../Services/API';
+import { firebaseAuth as firebase } from '../Services/Firebase/FirebaseConfig';
 
-// import loginUserSaga from './AuthSaga/loginUser';
+import registerUserSaga from './AuthSaga/registerUser';
 import getPriceSaga from './BankSaga/getPrice';
 import getBanksSaga from './BankSaga/getBanks';
 import sendTextSaga from './TwilioSaga/sendText';
 import sendTokenSaga from './StripeSaga/sendToken';
 
-// import { UserTypes } from '../Redux/UserRedux';
+import { AuthTypes } from '../Redux/AuthRedux';
 import { BankTypes } from '../Redux/BankRedux';
 import { TwilioTypes } from '../Redux/TwilioRedux';
 import { StripeTypes } from '../Redux/StripeRedux';
@@ -15,7 +16,8 @@ import { StripeTypes } from '../Redux/StripeRedux';
 const api = API.createAPI();
 
 export default function* rootSaga() {
-  yield [takeLatest(BankTypes.GET_ALL_BANKS, getBanksSaga, api),
+  yield [takeLatest(AuthTypes.REGISTER_USER, registerUserSaga, firebase),
+  takeLatest(BankTypes.GET_ALL_BANKS, getBanksSaga, api),
   takeLatest(BankTypes.GET_EXCHANGE_RATE, getPriceSaga, api),
   takeLatest(TwilioTypes.SEND_TEXT, sendTextSaga, api),
   takeLatest(StripeTypes.SEND_TOKEN, sendTokenSaga, api)];
