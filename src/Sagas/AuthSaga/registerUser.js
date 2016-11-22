9296cbddf4acd28b5163980a4da651fb05306d30
TobiahRex <tobiah.rex@gmail.com>
2016-11-22 13:36:29 -0800

    successfully have user registration information from firebase and error setup.  Need to create error handler Actions and server DB recording.

--- src/Sagas/AuthSaga/registerUser.js
+++ src/Sagas/AuthSaga/registerUser.js
@@ -1,8 +1,21 @@
 import { call, put } from 'redux-saga/effects';
 
 export default function* registerUser(firebase, { info }) {
-  console.log('firebase in registerUser\n ', firebase);
   const response = yield call(() =>
-  firebase.createUserWithEmailAndPassword(info.email, info.password));
-  console.log('response: ', response);
+  firebase.createUserWithEmailAndPassword(info.email, info.password)
+  .then(user => user)
+  .catch(err => err)
+);
+
+  if (response.uid) {
+    const { refreshToken, uid } = response;
+    console.log('providerData: ', );
+  } else {
+    const { code, message } = response;
+    /* Firebase Errors:
+    1. auth/weak-password - the password supplied is too short.
+    2. auth/already-in-use - the email used is already registered.
+    */
+    console.log('error: ', code, 'message: ', message);
+  }
 }
