83277243a4debc47c3d3e97db4c9f64621d67cfc
TobiahRex <tobiah.rex@gmail.com>
2016-11-23 21:36:40 -0800

    logoutUser is completed and ready for testing.

--- src/Sagas/AuthSaga/logoutUser.js
+++ src/Sagas/AuthSaga/logoutUser.js
@@ -7,9 +7,15 @@ export default function* logoutUser(firebase) {
   .then(() => ({
     ok: true,
     problem: false,
+  }))
+  .catch(() => ({
+    ok: false,
+    problem: 'Could not sign out user.',
   })));
 
   if (response.ok) {
     yield put(authActions.logoutUserSuccess());
+  } else {
+    yield put(authActions.logoutUserFail(response.problem));
   }
 }
