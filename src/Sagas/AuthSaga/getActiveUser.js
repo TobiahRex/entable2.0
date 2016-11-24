5a7d61f884ba840cb2393605060d008fa28995ad
TobiahRex <tobiah.rex@gmail.com>
2016-11-23 18:18:53 -0800

    finished draft of getActiveUser.js

--- src/Sagas/AuthSaga/getActiveUser.js
+++ src/Sagas/AuthSaga/getActiveUser.js
@@ -2,10 +2,28 @@ import { call, put } from 'redux-saga/effects';
 import authActions from '../../Redux/AuthRedux';
 import apiActions from '../../Redux/ApiRedux';
 
-export default function* getActiveUser(firebase) {
+export default function* getActiveUser(firebase, api) {
   const { currentUser } = firebase;
 
   if (currentUser) {
-    yield put(authActions.)
+    yield [
+      put(authActions.activeUserTrue()),
+      put(apiActions.fetching()),
+    ];
+
+    const response = yield call(() => api.getUser(currentUser.uid));
+    if (response.ok) {
+      yield [
+        put(authActions.getActiveUserSuccess(response.data)),
+        put(apiActions.apiSuccess()),
+      ];
+    } else {
+      yield [
+        put(authActions.getActiveUserFail(response.problem)),
+        put(apiActions.apiFail()),
+      ];
+    }
+  } else {
+    yield put(authActions.activeUserFalse());
   }
 }
