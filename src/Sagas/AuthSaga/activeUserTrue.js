b2d28fe7ecec2cbb57be53f8f247afe4e7416cac
TobiahRex <tobiah.rex@gmail.com>
2016-11-23 19:06:52 -0800

    modified filename for activeUserTrue.

---
+++ src/Sagas/AuthSaga/activeUserTrue.js
@@ -0,0 +1,22 @@
+import { call, put } from 'redux-saga/effects';
+import authActions from '../../Redux/AuthRedux';
+import apiActions from '../../Redux/ApiRedux';
+
+export default function* activeUserTrue(api, action) {
+  yield [
+    put(authActions.activeUserTrue()),
+    put(apiActions.fetching()),
+  ];
+  const response = yield call(() => api.getUser(action.user.uid));
+  if (response.ok) {
+    yield [
+      put(authActions.getActiveUserSuccess(response.data)),
+      put(apiActions.apiSuccess()),
+    ];
+  } else {
+    yield [
+      put(authActions.getActiveUserFail(response.problem)),
+      put(apiActions.apiFail()),
+    ];
+  }
+}
