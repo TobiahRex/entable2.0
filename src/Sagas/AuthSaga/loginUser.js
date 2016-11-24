0a7b02a522152d7feb77df1857e6712fbdf7e11c
TobiahRex <tobiah.rex@gmail.com>
2016-11-24 15:10:56 -0800

    modified loginUser.js saga to create a response object based on firebase login result.

--- src/Sagas/AuthSaga/loginUser.js
+++ src/Sagas/AuthSaga/loginUser.js
@@ -2,9 +2,18 @@ import { call, put } from 'redux-saga/effects';
 import userActions from '../../Redux/UserRedux';
 import apiActions from '../../Redux/ApiRedux';
 
-export default function* loginUser(api, { credentials }) {
-  const response = yield call(() => api.loginUser(credentials));
-
+export default function* loginUser(firebase, { credentials }) {
+  const response = yield call(() =>
+  firebase.signInWithEmailAndPassword(credentials.email, credentials.password)
+  .then(user => ({
+    ok: true,
+    user,
+  }))
+  .catch(error => ({
+    ok: false,
+    problem: error,
+  })));
+  console.log('response: ', response);
   if (response.ok) {
     yield [put(userActions.loginSuccess(response.data)),
     put(apiActions.apiSuccess())];
