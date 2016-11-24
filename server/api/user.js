4260ecb4f5b8056881c6fe15ab7367f99575c688
TobiahRex <tobiah.rex@gmail.com>
2016-11-23 20:04:28 -0800

    added method on User model to locate user by their firebase id.

--- server/api/user.js
+++ server/api/user.js
@@ -3,6 +3,9 @@ import User from '../models/dbModels/users';
 
 const router = new Router();
 
+router.route('/:id')
+.get((req, res) => User.find({ uid_firebase: req.params.id }, res.handle));
+
 router.route('/')
 .get((req, res) => User.find({}, res.handle))
 .delete((req, res) => User.remove({}, res.handle))
