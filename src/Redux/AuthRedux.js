aec2aa7cc294627b48730be80b6d323d344be675
TobiahRex <tobiah.rex@gmail.com>
2016-11-23 18:20:40 -0800

    modified action creators inside AuthRedux.js

--- src/Redux/AuthRedux.js
+++ src/Redux/AuthRedux.js
@@ -1,6 +1,7 @@
 import { createActions, createReducer } from 'reduxsauce';
 
 const nullFields = {
+  active: false,
   _id: null,
   uid_firebase: null,
   firstName: null,
@@ -16,8 +17,10 @@ const nullFields = {
 
 const { Types, Creators } = createActions({
   getActiveUser: null,
-  getActiveUserFail: null,
   getActiveUserSuccess: ['info'],
+  getActiveUserFail: ['error'],
+  activeUserFalse: null,
+  activeUserTrue: null,
   createUserFirebase: ['info'],
   createUserSuccess: ['user'],
   createUserFail: ['error'],
@@ -41,6 +44,7 @@ export const INITIAL_STATE = {
 };
 
 const userSuccess = (state, { user }) => ({
+  active: true,
   _id: user._id,
   uid_firebase: user.uid_firebase,
   firstName: user.firstName,
@@ -58,37 +62,42 @@ const userFail = (state, { error }) => ({ ...nullFields, error });
 
 const logoutSuccess = () => ({ ...INITIAL_STATE });
 const logoutFail = (state, { error }) => ({
-  _id: state._id,
-  uid_firebase: state.uid_firebase,
-  refreshToken: state.refreshToken,
-  firstName: state.firstName,
-  lastName: state.lastName,
-  email: state.email,
-  phone: state.phone,
-  lastLogin: state.lastLogin,
-  location: state.location,
-  photoUrl: state.photoUrl,
-  settings: state.settings,
+  active: state.user.active,
+  _id: state.user._id,
+  uid_firebase: state.user.uid_firebase,
+  refreshToken: state.user.refreshToken,
+  firstName: state.user.firstName,
+  lastName: state.user.lastName,
+  email: state.user.email,
+  phone: state.user.phone,
+  lastLogin: state.user.lastLogin,
+  location: state.user.location,
+  photoUrl: state.user.photoUrl,
+  settings: state.user.settings,
   error,
 });
 
 const loginFail = (state, { error }) => ({
-  _id: state._id,
-  uid_firebase: state.uid_firebase,
-  refreshToken: state.refreshToken,
-  firstName: state.firstName,
-  lastName: state.lastName,
-  email: state.email,
-  phone: state.phone,
-  lastLogin: state.lastLogin,
-  location: state.location,
-  photoUrl: state.photoUrl,
-  settings: state.settings,
+  active: state.user.active,
+  _id: state.user._id,
+  uid_firebase: state.user.uid_firebase,
+  refreshToken: state.user.refreshToken,
+  firstName: state.user.firstName,
+  lastName: state.user.lastName,
+  email: state.user.email,
+  phone: state.user.phone,
+  lastLogin: state.user.lastLogin,
+  location: state.user.location,
+  photoUrl: state.user.photoUrl,
+  settings: state.user.settings,
   error,
 });
 
 export const userReducer = createReducer(INITIAL_STATE, {
-
+  [Types.ACTIVE_USER_TRUE]: userSuccess,
+  [Types.ACTIVE_USER_FALSE]: userFail,
+  [Types.GET_ACTIVE_USER_SUCCESS]: userSuccess,
+  [Types.GET_ACTIVE_USER_FAIL]: userFail,
   [Types.CREATE_USER_SUCCESS]: userSuccess,
   [Types.CREATE_USER_FAIL]: userFail,
   [Types.SAVE_NEW_USER_SUCCESS]: userSuccess,
--- src/Sagas/index.js
+++ src/Sagas/index.js
@@ -2,6 +2,7 @@ import { takeLatest } from 'redux-saga';
 import API from '../Services/API';
 import { firebaseAuth } from '../Services/Firebase/FirebaseConfig';
 
+import getActiveUserSaga from './AuthSaga/getActiveUser';
 import createUserSaga from './AuthSaga/createUser';
 import getPriceSaga from './BankSaga/getPrice';
 import getBanksSaga from './BankSaga/getBanks';
@@ -17,9 +18,12 @@ const api = API.createAPI();
 const firebase = firebaseAuth;
 
 export default function* rootSaga() {
-  yield [takeLatest(AuthTypes.CREATE_USER_FIREBASE, createUserSaga, firebase, api),
-  takeLatest(BankTypes.GET_ALL_BANKS, getBanksSaga, api),
-  takeLatest(BankTypes.GET_EXCHANGE_RATE, getPriceSaga, api),
-  takeLatest(TwilioTypes.SEND_TEXT, sendTextSaga, api),
-  takeLatest(StripeTypes.SEND_TOKEN, sendTokenSaga, api)];
+  yield [
+    takeLatest(AuthTypes.GET_ACTIVE_USER, getActiveUserSaga, firebase, api),
+    takeLatest(AuthTypes.CREATE_USER_FIREBASE, createUserSaga, firebase, api),
+    takeLatest(BankTypes.GET_ALL_BANKS, getBanksSaga, api),
+    takeLatest(BankTypes.GET_EXCHANGE_RATE, getPriceSaga, api),
+    takeLatest(TwilioTypes.SEND_TEXT, sendTextSaga, api),
+    takeLatest(StripeTypes.SEND_TOKEN, sendTokenSaga, api),
+  ];
 }
