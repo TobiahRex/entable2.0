9816fe1f35dc2f4d5f650f66b379fd1d081c05ba
TobiahRex <tobiah.rex@gmail.com>
2016-11-23 16:55:23 -0800

    added some notes to createUser.js

--- src/Sagas/AuthSaga/createUser.js
+++ src/Sagas/AuthSaga/createUser.js
@@ -8,30 +8,40 @@ export default function* registerUser(firebase, api, { info }) {
   .then(user => user)
   .catch(err => err));
 
+  // if firebase successfully creates a new user...
   if (response.uid_firebase) {
     const { uid_firebase } = response;
     const newUserInfo = { ...info, uid_firebase };
 
+    /* dispatch the following actions...
+    1. success action for firebase creating new user
+    2. an api fetching actions in lieu of the next action
+    3. a local database saving action.
+    */
     yield [
       put(authActions.createUserSuccess(newUserInfo)),
       put(apiActions.fetching()),
       put(authActions.saveNewUser(newUserInfo)),
     ];
 
+    // call an asynch api action for saving a new user
     const dbResponse = yield call(() => api.saveNewUser(newUserInfo));
 
     if (dbResponse.ok) {
+      // dispatch the respective succcess actions
       yield [
         put(authActions.saveNewUserSuccess(dbResponse.data)),
         put(apiActions.apiSuccess()),
       ];
     } else {
+      // dispatch the respective failing actions
       yield [
         put(authActions.saveNewUserFail(response.problem)),
         put(apiActions.apiFail()),
       ];
     }
   } else {
+    // dispatch the failing action for creating a new user from firebase.
     const { code, message } = response;
     /* Firebase Errors:
     1. auth/weak-password - the password supplied is too short.
