1d1dd4e882b19147998696fb03c9a4c2ff23b8b7
TobiahRex <tobiah.rex@gmail.com>
2016-11-24 13:33:59 -0800

    functionality wireframe built on loginContainer.js

--- src/Containers/Login/loginContainer.js
+++ src/Containers/Login/loginContainer.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import authActions from '../../Redux/AuthRedux';
 
 class Login extends React.Component {
+  static propTypes = {
+    login: PropTypes.func.isRequired,
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       username: '',
       password: '',
     };
+  }
 
-    render() {
-      return (
-        <div>
-          <h1>Login Page</h1>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+      </div>
+    );
   }
 }
 const mapDispatchToProps = dispatch => ({
-  login: (credentials) => dispatch(authActions.loginUser(credentials)),
+  login: credentials => dispatch(authActions.loginUser(credentials)),
 });
+export default connect(null, mapDispatchToProps)(Login);
