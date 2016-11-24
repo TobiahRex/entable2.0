14b49ff3c46656db297487e72e4706e67fa6929f
TobiahRex <tobiah.rex@gmail.com>
2016-11-23 21:47:23 -0800

    converted dumb component Navbar into a React.PureComponent to mapDispatchToProps.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,61 +1,66 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import authActions from '../Redux/AuthRedux';
 
-const Navbar = ({ children }) => (
-  <div>
-    <nav className="navbar navbar-inverse navbar-default">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-            aria-expanded="false"
-          >
-            <span className="sr-only" />
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-          </button>
-          <a className="navbar-brand" href="/">
-            <img
-              src="/entable_logo_final_white.png"
-              height="20px"
-              role="presentation"
-            />
-          </a>
-        </div>
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul className="nav navbar-nav navbar-right">
-            <li className="mainNavLink">
-              <Link to="/example">Bank Example</Link>
-            </li>
-            <li className="mainNavLink">
-              <Link to="/register">
-                Become a Donor
-              </Link>
-            </li>
-            <li className="mainNavLink">
-              <button onClick={this.props.logoutUser}>
-                Log Out
+class Navbar extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    logoutUser: PropTypes.func.isRequired,
+  };
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-inverse navbar-default">
+          <div className="container">
+            <div className="navbar-header">
+              <button
+                type="button"
+                className="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false"
+              >
+                <span className="sr-only" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
               </button>
-            </li>
-          </ul>
-        </div>
+              <a className="navbar-brand" href="/">
+                <img
+                  src="/entable_logo_final_white.png"
+                  height="20px"
+                  role="presentation"
+                />
+              </a>
+            </div>
+            <div
+              className="collapse navbar-collapse"
+              id="bs-example-navbar-collapse-1"
+            >
+              <ul className="nav navbar-nav navbar-right">
+                <li className="mainNavLink">
+                  <Link to="/example">Bank Example</Link>
+                </li>
+                <li className="mainNavLink">
+                  <Link to="/register">
+                    Become a Donor
+                  </Link>
+                </li>
+                <li className="mainNavLink">
+                  <a href="" onClick={this.props.logoutUser}>
+                    Log Out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        {this.props.children}
       </div>
-    </nav>
-    {children}
-  </div>
-);
-Navbar.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+    );
+  }
+}
 const mapDispatchToProps = dispatch => ({
   logoutUser: () => dispatch(authActions.logoutUser()),
 });
