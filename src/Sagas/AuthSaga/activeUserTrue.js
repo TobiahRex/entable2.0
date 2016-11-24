3493b4c9e443a6445dc7e00ffb51cc764882cc08
TobiahRex <tobiah.rex@gmail.com>
2016-11-23 19:08:51 -0800

    destructured user out of action for activeUserTrue.js and modified action creators inside AuthRedux.js

--- src/Sagas/AuthSaga/activeUserTrue.js
+++ src/Sagas/AuthSaga/activeUserTrue.js
@@ -2,12 +2,12 @@ import { call, put } from 'redux-saga/effects';
 import authActions from '../../Redux/AuthRedux';
 import apiActions from '../../Redux/ApiRedux';
 
-export default function* activeUserTrue(api, action) {
+export default function* activeUserTrue(api, { user }) {
   yield [
     put(authActions.activeUserTrue()),
     put(apiActions.fetching()),
   ];
-  const response = yield call(() => api.getUser(action.user.uid));
+  const response = yield call(() => api.getUser(user.uid));
   if (response.ok) {
     yield [
       put(authActions.getActiveUserSuccess(response.data)),
