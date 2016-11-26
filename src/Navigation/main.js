172c0766d55dcb6648bcde9031e33e9ff849005e
TobiahRex <tobiah.rex@gmail.com>
2016-11-25 16:53:23 -0800

    added "manager_account" and "donor_account" to main Navigation routes.

--- src/Navigation/main.js
+++ src/Navigation/main.js
@@ -7,7 +7,7 @@ import BankDetail from '../Components/Bank/BankDetail';
 import BankExample from '../Components/Bank/BankExample';
 import LoginPage from '../Containers/Login/loginContainer';
 import RegisterPage from '../Containers/Register/registerContainer';
-import DonatePg from '../Containers/DonatePg';
+import DonorPage from '../Containers/DonatePg';
 
 import donateBank from '../Components/Donation/donateBank';
 import donateGen from '../Components/Donation/donateGen';
@@ -21,7 +21,8 @@ export default (
     <Router path="example" component={BankExample} />
     <Router path="login" component={LoginPage} />
     <Router path="register" component={RegisterPage} />
-    <Router path="donor_account" component={DonatePg} />
+    <Router path="donor_account" component={DonorPage} />
+    {/* <Router path="manager_account" component={ManagerPage} /> */}
     <Router path="sponsor" component={donateGen} />
     <Router path="donation/:id" component={donateBank} />
     <Router path="flex" component={flexBoxTest} />
