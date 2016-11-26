06d86f86d690e6f752988a7bc95c0e00867f00b7
TobiahRex <tobiah.rex@gmail.com>
2016-11-25 19:55:11 -0800

    escaped apostrophes inside DonatePg.js & added h2 banner.

--- src/Containers/DonatePg.js
+++ src/Containers/DonatePg.js
@@ -42,7 +42,9 @@ class DonationPg extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello {this.props.name.firstName}</h1>
+        <h1>Hello {this.props.name.firstName}, welcome back!</h1>
+        <br />
+        <h2>{'Here\'s'} {'what\'s'} happened since {'you\'ve'} been away.</h2>
       </div>
     );
   }
