364438471c9fbf0528029b1dfc2be11507569d07
TobiahRex <tobiah.rex@gmail.com>
2016-11-27 19:36:03 -0800

    completed moving styles from donate page to carouselStyles.js

--- src/Components/Carousel/carouselStyles.js
+++ src/Components/Carousel/carouselStyles.js
@@ -7,15 +7,20 @@ const styles = {
     textShadow: '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
   },
   image: {
-
+    padding: 25,
+    backgroundColor: '#222',
   },
   captionContainer: {
+    backgroundColor: '#2ecc71',
+    padding: 5,
   },
   captionH3: {
-
+    margin: 0,
   },
   captionDesc: {
-
+    padding: 5,
+    color: '#f5f5f5',
+    margin: 0,
   },
-}
+};
 export default styles;
