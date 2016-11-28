89ef751b2f5f3f515c11f6dabe0cddfc4e7dd09d
TobiahRex <tobiah.rex@gmail.com>
2016-11-27 19:32:03 -0800

    created a seperate component for Carousel.

---
+++ src/Components/Carousel/carouselMain.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/lib';
+
+class CarouselMain extends React.PureComponent {
+  render() {
+    return (
+      <Carousel>
+        hi
+      </Carousel>
+    );
+  }
+}
+export default CarouselMain;
