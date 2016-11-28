65afb91617b2ece606cf071ea7ba4907886d818c
TobiahRex <tobiah.rex@gmail.com>
2016-11-27 19:34:55 -0800

    created carouselStyles.js and added style props.

---
+++ src/Components/Carousel/carouselStyles.js
@@ -0,0 +1,21 @@
+const styles = {
+  h3: {
+    margin: 0,
+    backgroundColor: '#2ecc71',
+    textAlign: 'center',
+    padding: 10,
+    textShadow: '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+  },
+  image: {
+
+  },
+  captionContainer: {
+  },
+  captionH3: {
+
+  },
+  captionDesc: {
+
+  },
+}
+export default styles;
