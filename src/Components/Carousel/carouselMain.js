b9cb946048dc10f161595266ee118e589e28d807
TobiahRex <tobiah.rex@gmail.com>
2016-11-27 19:37:21 -0800

    imported & added styles to carouselMain.js

--- src/Components/Carousel/carouselMain.js
+++ src/Components/Carousel/carouselMain.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/lib';
-import carouselStyle from './carouselStyles';
+import styles from './carouselStyles';
 
 class CarouselMain extends React.PureComponent {
   render() {
     return (
       <Carousel>
         <Carousel.Item>
-          <h3 style={DonationPg.styles.h3Carousel}>Bank of Tanzania</h3>
+          <h3 style={styles.h3Carousel}>Bank of Tanzania</h3>
           <img
-            style={DonationPg.styles.imgCarousel}
+            style={styles.imgCarousel}
             width={900}
             height={500}
             alt="900x500"
             src="./girl-469157.jpg"
           />
           <Carousel.Caption>
-            <div style={DonationPg.styles.capContainCarousel}>
-              <h3 style={DonationPg.styles.carouselH3}>Nulla Wambosi</h3>
-              <p style={DonationPg.styles.pCarousel}>
+            <div style={styles.capContainCarousel}>
+              <h3 style={styles.carouselH3}>Nulla Wambosi</h3>
+              <p style={styles.pCarousel}>
                 Nulla purchased fresh meat and produce for her family with 3500 MPESA on October 21nd, 2016.
               </p>
             </div>
@@ -28,4 +28,3 @@ class CarouselMain extends React.PureComponent {
     );
   }
 }
-export default CarouselMain;
