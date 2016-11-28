be404638d88c4c895f729b11fb9063c6bf47cee3
TobiahRex <tobiah.rex@gmail.com>
2016-11-28 13:03:55 -0800

    created "renderCarousel" inside carouselMain.js

--- src/Components/Carousel/carouselMain.js
+++ src/Components/Carousel/carouselMain.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Carousel from 'react-bootstrap/lib';
 import styles from './carouselStyles';
 
 class CarouselMain extends React.PureComponent {
-  renderItem = () => {
-    this.props.transactions.map((trans, i) => {
-      <Carousel.Item
-      >
+  static propTypes = {
+    transactions: PropTypes.arrayOf(PropTypes.any),
+  }
+  renderCarousel = () => {
+    this.props.transactions.map((trans, i) => (
+      <Carousel.Item key={`carousel${i}`}>
         <h3 style={styles.h3Carousel}>{trans.bank}</h3>
         <img
           style={styles.imgCarousel}
@@ -15,8 +17,16 @@ class CarouselMain extends React.PureComponent {
           width={900}
           height={500}
         />
+        <Carousel.Caption>
+          <div style={styles.capContainCarousel}>
+            <h3 style={styles.carouselH3}>{trans.name}</h3>
+            <p style={styles.pCarousel}>
+              {trans.description}
+            </p>
+          </div>
+        </Carousel.Caption>
       </Carousel.Item>
-    });
+    ));
   }
 
   render() {
