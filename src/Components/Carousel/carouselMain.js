efb3793f92591f3a0684ebe5e00f6fe96c0bb326
TobiahRex <tobiah.rex@gmail.com>
2016-11-28 13:06:06 -0800

    added key prop to Carousel.Item render function inside carouselMain.js

--- src/Components/Carousel/carouselMain.js
+++ src/Components/Carousel/carouselMain.js
@@ -4,11 +4,11 @@ import styles from './carouselStyles';
 
 class CarouselMain extends React.PureComponent {
   static propTypes = {
-    transactions: PropTypes.arrayOf(PropTypes.any),
+    transactions: PropTypes.arrayOf(PropTypes.object),
   }
   renderCarousel = () => {
     this.props.transactions.map((trans, i) => (
-      <Carousel.Item key={`carousel${i}`}>
+      <Carousel.Item key={`carousel-${i}`}>
         <h3 style={styles.h3Carousel}>{trans.bank}</h3>
         <img
           style={styles.imgCarousel}
@@ -31,26 +31,9 @@ class CarouselMain extends React.PureComponent {
 
   render() {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <h3 style={styles.h3Carousel}>Bank of Tanzania</h3>
-          <img
-            style={styles.imgCarousel}
-            width={900}
-            height={500}
-            alt="900x500"
-            src="./girl-469157.jpg"
-          />
-          <Carousel.Caption>
-            <div style={styles.capContainCarousel}>
-              <h3 style={styles.carouselH3}>Nulla Wambosi</h3>
-              <p style={styles.pCarousel}>
-                Nulla purchased fresh meat and produce for her family with 3500 MPESA on October 21nd, 2016.
-              </p>
-            </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <span>
+        {this.renderCarousel()}
+      </span>
     );
   }
 }
