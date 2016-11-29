f66fb6892cdbdeeb9e42bde558351eaedccdced2
TobiahRex <tobiah.rex@gmail.com>
2016-11-28 18:35:25 -0800

    added a redirect to "/" after user logs out.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { PropTypes, PureComponent } from 'react';
+import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import authActions from '../Redux/AuthRedux';
@@ -112,6 +113,9 @@ const mapStateToProps = state => ({
   role: state.user.role,
 });
 const mapDispatchToProps = dispatch => ({
-  logoutUser: () => dispatch(authActions.logoutUser()),
+  logoutUser: () => {
+    dispatch(authActions.logoutUser());
+    browserHistory.push('/');
+  },
 });
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
