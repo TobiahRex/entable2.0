10e0b929e47de66ded8d839a46cb4437c7c07a75
TobiahRex <tobiah.rex@gmail.com>
2016-11-30 23:19:59 -0800

    added componentWillMount & componentWillReceiveProps lifeCycle methods to managerContainer.js

--- src/Containers/Manager/managerContainer.js
+++ src/Containers/Manager/managerContainer.js
@@ -12,10 +12,31 @@ class ManagerContainer extends React.Component {
       manager: null,
     };
   }
-  
-}
 
+  componentWillMount() {
+    this.props.findBankManager();
+  }
+
+  componentWillReceiveProps({ manager }) {
+    if (this.state.manager !== manager) {
+      this.setState({ manager });
+    }
+  }
+
+  render() {
+    return (
+      <div id="managerContainer">
+        This is the manager Container.
+      </div>
+    );
+  }
+}
+const mapStateToProps = ({ manager }) => ({
+  manager: manager._id,
+  bank: manager.bank,
+  manager_active: manager.manager_active,
+});
 const mapDispatchToProps = (dispatch, { routeParams }) => ({
   findBankManger: () => dispatch(managerActions.findBankManager(routeParams.id)),
 });
-export default connect(null, mapDispatchToProps)(ManagerContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ManagerContainer);
--- src/Redux/index.js
+++ src/Redux/index.js
@@ -7,10 +7,12 @@ import { apiReducer as api } from './ApiRedux';
 import { bankReducer as bank } from './BankRedux';
 import { twilioReducer as twilio } from './TwilioRedux';
 import { donationReducer as donation } from './DonationRedux';
+import { managerReducer as manager } from './ManagerRedux';
 
 export default () => {
   const rootReducer = combineReducers({
     user,
+    manager,
     api,
     donation,
     twilio,
