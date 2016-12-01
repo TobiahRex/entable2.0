9d11be74f56b36a06c9465093854c72315cafb37
TobiahRex <tobiah.rex@gmail.com>
2016-11-30 23:05:09 -0800

    finished writing ManagerRedux.js - UNTESTED.

--- src/Redux/ManagerRedux.js
+++ src/Redux/ManagerRedux.js
@@ -2,9 +2,35 @@ import { createActions, createReducer } from 'reduxsauce';
 
 const { Types, Creators } = createActions({
   findBankManager: ['managerId'],
-  findBankManagerSuccess: ['manager'],
+  findBankManagerSuccess: ['bank'],
   findBankManagerFail: ['error'],
 });
 
 export default Creators;
 export const ManagerTypes = Types;
+export const INITIAL_STATE = {
+  _id: null,
+  manager_active: false,
+  bank: null,
+  error: null,
+};
+
+
+const success = (state, { bank }) => ({
+  _id: bank[0]._id,
+  manager_active: true,
+  bank: bank[0],
+  error: null,
+});
+
+const fail = (state, { error }) => ({
+  _id: null,
+  manager_active: false,
+  bank: null,
+  error,
+});
+
+export const managerReducer = createReducer(INITIAL_STATE, {
+  [Types.FIND_BANK_MANAGER_SUCCESS]: success,
+  [Types.FIND_BANK_MANAGER_FAIL]: fail,
+});
