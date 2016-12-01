94a9b2ea30a3378f2551578bfe66be8391504940
TobiahRex <tobiah.rex@gmail.com>
2016-11-30 22:45:03 -0800

    added findBankManagerSaga to rootSaga.

--- src/Sagas/index.js
+++ src/Sagas/index.js
@@ -10,17 +10,20 @@ import getPriceSaga from './BankSaga/getPrice';
 import getBanksSaga from './BankSaga/getBanks';
 import sendTextSaga from './TwilioSaga/sendText';
 import sendTokenSaga from './StripeSaga/sendToken';
+import findBankManagerSaga from './ManagerSaga/findBankManagerSaga';
 
 import { AuthTypes } from '../Redux/AuthRedux';
 import { BankTypes } from '../Redux/BankRedux';
 import { TwilioTypes } from '../Redux/TwilioRedux';
 import { StripeTypes } from '../Redux/StripeRedux';
+import { ManagerTypes } from '../Redux/ManagerRedux';
 
 const api = API.createAPI();
 const firebase = firebaseAuth;
 
 export default function* rootSaga() {
   yield [
+    takeLatest(ManagerTypes.FIND_BANK_MANAGER, findBankManagerSaga, api),
     takeLatest(AuthTypes.LOGIN_USER, loginUserSaga, firebase, api),
     takeLatest(AuthTypes.LOGOUT_USER, logoutUserSaga, firebase),
     takeLatest(AuthTypes.ACTIVE_USER_TRUE, activeUserTrueSaga, api),
