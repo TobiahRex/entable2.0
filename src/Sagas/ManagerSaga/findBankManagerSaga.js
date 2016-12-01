0c97633f42f5aa68f1ed358045365d4358145613
TobiahRex <tobiah.rex@gmail.com>
2016-11-30 22:43:32 -0800

    created psudeo code in findBankManagerSaga.js

---
+++ src/Sagas/ManagerSaga/findBankManagerSaga.js
@@ -0,0 +1,19 @@
+import { call, put } from 'redux-saga/effects';
+import managerActions from '../../Redux/ManagerRedux';
+import apiActions from '../../Redux/ApiRedux';
+
+export default function* (api, { managerId }) {
+  const response = yield call(() => api.findBankManager(managerId));
+
+  if (response.ok) {
+    yield [
+      put(apiActions.apiSuccess()),
+      put(managerActions.findBankManagerSuccess(response.data)),
+    ];
+  } else {
+    yield [
+      put(apiActions.apiFail()),
+      put(managerActions.findBankManagerFail(response.problem)),
+    ];
+  }
+}
