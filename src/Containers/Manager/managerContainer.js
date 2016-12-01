e22e87355689f45537f218b65ddfcb905a1c6e2e
TobiahRex <tobiah.rex@gmail.com>
2016-11-30 23:10:15 -0800

    building out ManagerContainer.js

--- src/Containers/Manager/managerContainer.js
+++ src/Containers/Manager/managerContainer.js
@@ -1,1 +1,21 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import managerActions from '../../Redux/ManagerRedux';
+
+class ManagerContainer extends React.Component {
+  static propTypes = {
+    findBankManager: PropTypes.func.isRequired,
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      manager: null,
+    };
+  }
+  
+}
+
+const mapDispatchToProps = (dispatch, { routeParams }) => ({
+  findBankManger: () => dispatch(managerActions.findBankManager(routeParams.id)),
+});
+export default connect(null, mapDispatchToProps)(ManagerContainer);
