45103630274888637915bca7c156c83996fd6620
TobiahRex <tobiah.rex@gmail.com>
2016-12-03 18:28:10 -0800

    created a rough version of ReDonateButton.js

--- src/Components/Donation/ReDonateButtons.js
+++ src/Components/Donation/ReDonateButtons.js
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
 import { CircularProgress } from 'material-ui';
 
+console.log('CircularProgress: ', CircularProgress);
+
 const reDonateButtons = ({ reDonate }) => (
   <div className="redonateOptions">
     <div className="redonateContainer">
@@ -10,7 +12,11 @@ const reDonateButtons = ({ reDonate }) => (
       >Re-Donate</button>
     </div>
     <div className="lodaingContainer">
-
+      <CircularProgress size={80} thickness={5} />
     </div>
   </div>
-)
+);
+reDonateButtons.propTypes = {
+  reDonate: PropTypes.func,
+};
+export default reDonateButtons;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import createStore from './Redux/index';
 import routes from './Navigation/main';
 
+injectTapEventPlugin();
 const store = createStore();
 render(
   <Provider store={store}>
