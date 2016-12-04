de4a93680f0afcf082174502a1e2f1ffb3465f69
TobiahRex <tobiah.rex@gmail.com>
2016-12-03 18:21:56 -0800

    installed material-ui for importing the circularLoadingIcon.

--- src/Components/Donation/ReDonateButtons.js
+++ src/Components/Donation/ReDonateButtons.js
@@ -1,5 +1,16 @@
 import React, { PropTypes } from 'react';
+import { CircularProgress } from 'material-ui';
 
 const reDonateButtons = ({ reDonate }) => (
-  
+  <div className="redonateOptions">
+    <div className="redonateContainer">
+      <button
+        className="redonateButton"
+        onClick={e => reDonate(e)}
+      >Re-Donate</button>
+    </div>
+    <div className="lodaingContainer">
+
+    </div>
+  </div>
 )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import createStore from './Redux/index';
 import routes from './Navigation/main';
