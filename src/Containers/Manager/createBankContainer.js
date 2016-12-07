64088f4f415e9471ab2da9fe5f121a716d1e7b5d
TobiahRex <tobiah.rex@gmail.com>
2016-12-06 19:39:57 -0800

    removed unnecessary components in createBankContainer.js

--- src/Containers/Manager/createBankContainer.js
+++ src/Containers/Manager/createBankContainer.js
@@ -54,125 +54,46 @@ class CreateBank extends React.Component {
                 <input type="text" id="bankCountry" />
               </div>
               <div style={CreateBank.styles.bankCityInput}>
-                <label htmlFor="bankCity">Bank City</label>
+                <label htmlFor="bankCity">City</label>
                 <input type="text" id="bankCity" />
               </div>
               <div style={CreateBank.styles.bankPhoneInput}>
-                <label htmlFor="bankPhone">Bank Phone</label>
+                <label htmlFor="bankPhone">Phone</label>
                 <input type="text" id="bankPhone" />
               </div>
             </div>
 
           </div>
-          <div id="addTransContainer">
-            <div>
-              <button onClick={() => console.log('click')}>
-                {ddButtonTitle}
-              </button>
-            </div>
+          <div>
+            <label htmlFor="transPhoto">
+              Transaction Photo
+            </label>
+            <input
+              id="transPhoto"
+              type="text"
+              value={this.state.photoUrl}
+              onChange={e =>
+                this.setState({ photoUrl: e.target.value })
+              }
+            />
           </div>
-          <div id="hiddenDropDown">
-            <div id="dropDownInputContainer">
-              <div>
-                <label htmlFor="bankName">
-                  Bank Name
-                </label>
-                <input
-                  id="bankName"
-                  type="text"
-                  disabled value={bankName}
-                />
-              </div>
-              <div>
-                <label htmlFor="transDate">
-                  Date
-                </label>
-                <input
-                  id="transDate"
-                  type="text"
-                  disabled
-                  value={currentDate}
-                />
-              </div>
-              <div>
-                <label htmlFor="member">
-                  Member Name
-                </label>
-                <input
-                  id="member"
-                  type="text"
-                  value={this.state.transMember}
-                  onChange={e =>
-                    this.setState({ member: e.target.value })
-                  }
-                />
-              </div>
-              <div>
-                <label htmlFor="transDesc">
-                  Transaction Description
-                </label>
-                <input
-                  id="transDesc"
-                  type="text"
-                  value={bankName}
-                  onChange={e =>
-                    this.setState({ description: e.target.value })
-                  }
-                />
-              </div>
-              <div>
-                <label htmlFor="transAmount">
-                  Transaction Amount
-                </label>
-                <input
-                  id="transAmount"
-                  type="text"
-                  value={this.state.amount}
-                  onChange={e =>
-                    this.setState({ amount: e.target.value })
-                  }
-                />
-              </div>
-              <div>
-                <label htmlFor="transPhoto">
-                  Transaction Photo
-                </label>
-                <input
-                  id="transPhoto"
-                  type="text"
-                  value={this.state.photoUrl}
-                  onChange={e =>
-                    this.setState({ photoUrl: e.target.value })
-                  }
-                />
-              </div>
-              <div>
-                <label htmlFor="transLegal">
-                  Legal Agreement
-                </label>
-                <input
-                  id="transLegal"
-                  type="checkbox"
-                  value={this.state.agreement}
-                  onChange={e =>
-                    this.setState({ agreement: e.target.value })
-                  }
-                />
-              </div>
-              <div id="submitTransaction">
-                <button onClick={() => console.log('submit transaction')}>
-                  Submit
-                </button>
-              </div>
-            </div>
+          <div>
+            <label htmlFor="transLegal">
+              Legal Agreement
+            </label>
+            <input
+              id="transLegal"
+              type="checkbox"
+              value={this.state.agreement}
+              onChange={e =>
+                this.setState({ agreement: e.target.value })
+              }
+            />
           </div>
-          <div id="bankHistoryTitle">
-            <h4>
-              Bank History & Notes for Donors
-            </h4>
-          </div>
-          <div id="bankHistoryContainer">
-            {'this.renderHistory()'}
+          <div id="submitTransaction">
+            <button onClick={() => console.log('submit transaction')}>
+              Submit
+            </button>
           </div>
         </div>
       </div>
