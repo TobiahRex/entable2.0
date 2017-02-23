9d18434a66eb2755338a438908e51b610e5c89d9
TobiahRex <tobiah.rex@gmail.com>
2017-02-23 22:07:06 +0900

    installed image-webpack loader and optimized query parameters.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -53,9 +53,26 @@ module.exports = {
       {
         test: /\.(jpe?g|png|giff|svg|ico)$/i,
         loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
-        ],
+          'file?hash=sha512&digest=hex&name=[hash].[ext]', {
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 4,
+              },
+              pngquant: {
+                quality: '75-90',
+                speed: 3,
+              },
+            },
+          }],
+        exclude: /node_modules/,
+        include: __dirname,
       },
     ],
   },
